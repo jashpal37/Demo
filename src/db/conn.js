d5fc3f98f7dcbcaaa406331a142c0949b91ab602
jashpal37 <87304803+jashpal37@users.noreply.github.com>
2023-02-03 11:47:07 +0530

    connect with mongoDB Atlas

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,4 +1,25 @@
 const mongoose = require('mongoose');
+
+const dbUrl = "mongodb+srv://demo12:123123123@cluster0.ovfnci1.mongodb.net/demo?retryWrites=true&w=majority"
+
+const connectionParams = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
+
+
+mongoose.set('strictQuery', true);
+mongoose.connect(dbUrl,connectionParams)
+.then(() => {
+    console.log('Connected to MongoDB Atlas');
+})
+.catch((err) => {
+    console.log(err);
+});  
+
+
+/*
+const mongoose = require('mongoose');
 mongoose.set('strictQuery', true);
 mongoose.connect('mongodb://localhost:27017/Users',
 {
@@ -9,3 +30,4 @@ mongoose.connect('mongodb://localhost:27017/Users',
 }).catch((err) => {
     console.log(err);
 });  
+*/
